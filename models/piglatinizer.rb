2dffededaa059eac5e4130bc7f73fde7bc5f01ee
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-17 19:20:22 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,17 +6,18 @@ class PigLatinizer
   end
 
   def translate
+    binding.pry
     words = text.split(/\W+/)
     words.map! do | word |
       word.upcase
     end
 #    map word array
-#    if 1st letter with multiple consonants 
-#      word starting at 1st non-consonant position1 concat 1st letter and concat ay    
+#    if 1st letter with multiple consonants
+#      word starting at 1st non-consonant position1 concat 1st letter and concat ay
 #    if 1st letter is consonant
 #        word starting at position1 concat 1st letter and concat ay
 #    if 1st letter vowel
-#        word + yay 
+#        word + yay
     words.join(' ')
   end
 
